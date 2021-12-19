a19a6b083683430925b800e501cf5237967c9f4c
Mariam Hossam <73313364+bangkook@users.noreply.github.com>
2021-12-19 21:29:55 +0200

    Update main.c

--- dots and boxes/main.c
+++ dots and boxes/main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#include "Grid.h"
+#include "GameLoop.h"
+#define red FOREGROUND_RED
+#define blue FOREGROUND_BLUE
+#define green FOREGROUND_GREEN
 
-#include "GridDisplay.h"
+int switchPlayer(int playerNo){
+    if(playerNo == 1) playerNo = 2;
+    else playerNo = 1;
+    return playerNo;
+}
+
 
 int main(){
+
     int len = 2;
     UI(len, len);
+
+    //system("cls");
+    GameLoop(12);
+
 
     return 0;
 }

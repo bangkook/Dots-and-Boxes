4380cb82e374cb7a077acbfcaf0b8ff9bc87765d
Mariam Hossam <73313364+bangkook@users.noreply.github.com>
2021-12-21 22:29:41 +0200

    Add files via upload

---
+++ dots and boxes/computer.h
@@ -0,0 +1,26 @@
+void computerTurn(int size, char (*grid)[size], char *r1, char *r2, char *c1, char *c2){
+
+    for( int i=0; i<size; i++){
+
+        for(int j=0; j<size; j++){
+
+            for(int x=0; x<size; x++){
+
+                for(int y=0; y<size; y++){
+
+                    if(grid[i][j] == '*' && grid[x][y] == '*' && grid[(i+x)/2][(j+y)/2] == ' '){
+                        if(i == x && abs(j-y) == 2 || j == y && abs(i-x) == 2){
+                        *r1 = i;
+                        *c1 = j;
+                        *r2 = x;
+                        *c2 = y;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}

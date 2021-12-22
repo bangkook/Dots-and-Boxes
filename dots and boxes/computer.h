bd87fb347d5f0f071331d18af9b6ef5096b1d10f
Mariam Hossam <73313364+bangkook@users.noreply.github.com>
2021-12-22 17:56:13 +0200

    Update computer.h

--- dots and boxes/computer.h
+++ dots and boxes/computer.h
@@ -8,12 +8,12 @@ void computerTurn(int size, char (*grid)[size], char *r1, char *r2, char *c1, ch
 
                 for(int y=0; y<size; y++){
 
-                    if(grid[i][j] == '*' && grid[x][y] == '*' && grid[(i+x)/2][(j+y)/2] == ' '){
+                    if(i%2==0 && j%2==0 && x%2==0 && y%2==0 && grid[(i+x)/2][(j+y)/2] == ' '){
                         if(i == x && abs(j-y) == 2 || j == y && abs(i-x) == 2){
-                        *r1 = i;
-                        *c1 = j;
-                        *r2 = x;
-                        *c2 = y;
+                            *r1 = i;
+                            *c1 = j;
+                            *r2 = x;
+                            *c2 = y;
                         }
                     }
 

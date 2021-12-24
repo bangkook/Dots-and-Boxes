82abf330fd5676b01f48616833f64c22f852db3c
Mariam Hossam <73313364+bangkook@users.noreply.github.com>
2021-12-24 12:56:27 +0200

    Update MainMenu.h

--- dots and boxes/MainMenu.h
+++ dots and boxes/MainMenu.h
@@ -1,32 +1,55 @@
 void mainMenu(){
-printf("Enter 0 to start\n");
-   printf("Enter 1 to load a game\n");
-   printf("Enter 2 to view  Top 10 players\n");
-   printf("Enter 3 to exit\n");
+    setTextColor(green);
+    printf("\n\t\t\t\tWelcome to\n\t\t\t      Dots and Boxes\n\n");
+    setTextColor(reset);
+   printf(" Enter 0 to start\n");
+   printf(" Enter 1 to load a game\n");
+   printf(" Enter 2 to view  Top 10 players\n");
+   printf(" Enter 3 to exit\n\n ");
    int x;
    scanOfx:
    scanf("%i",&x);
 
    switch(x){
 
-       case 0:scanLevel:
-              system("cls");
-              printf("Enter 0 for beginner\n");
-              printf("Enter 1 for expert\n");
-              int level;
+       case 0:system("cls");
+              scanLevel:
+              printf("\nEnter 0 for beginner\n");
+              printf("\nEnter 1 for medium\n\nEnter 2 for expert\n\n");
               scanf("%i",&level);
+              if(level == 0) size = 2 * easy + 1;
+              else if (level == 1) size = 2 * medium + 1;
+              else if (level == 2) size = 2 * expert + 1;
               system("cls");
-              if(level!=0 && level!=1){
+              if(level!=0 && level!=1 && level!=2){
                   printf("invalid number.Please, enter another number\n");
                   goto scanLevel;
               }
 
               scanNOP:
-              printf("Enter 0 for playing aginst the computer\n");
-              printf("Enter 1 for 2 players\n");
+              printf("\nEnter 0 for playing against the computer\n");
+              printf("\nEnter 1 for 2 players\n\n");
               int numOfPlayers;
               scanf("%i",&numOfPlayers);
               system("cls");
+              if(numOfPlayers==0 ){
+                  mode = 0;
+                  printf("Please, enter your name: ");
+                  scanf("%s",&player1.name);
+                  player2 = computer;
+                  system("cls");
+              }
+              if(numOfPlayers==1 ){
+                  mode = 1;
+                  setTextColor(red);
+                  printf("Please, enter player1 first name: ");
+                  scanf("%s",&player1.name);
+                  setTextColor(blue);
+                  printf("\nPlease, enter player2 first name: ");
+                  scanf("%s",&player2.name);
+                  setTextColor(reset);
+                  system("cls");
+              }
               if(numOfPlayers!=0 && numOfPlayers!=1){
                   printf("invalid number.Please, enter another number\n");
                   goto scanNOP;
@@ -35,10 +58,12 @@ printf("Enter 0 to start\n");
                break;
 
       /* case 1:
+
        case 2:*/
 
        case 3: printf("bye");
-               return 0;
+               exit(1);
+
 
        default:
        printf("invalid number.Please enter another number\n");
